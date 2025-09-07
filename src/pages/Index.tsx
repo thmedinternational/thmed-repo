27c9740b61b3d236fbddc601a30613c06659ac0e
[dyad] <sueguardmanagement@gmail.com>
2025-09-07 10:49:51 +0200

    [dyad] Temporarily removed BannerSection from Index.tsx to diagnose dashboard white screen - wrote 1 file(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,11 +1,11 @@
 import ProductList from "@/components/ProductList";
-import BannerSection from "@/components/BannerSection"; // Import the new banner component
+// import BannerSection from "@/components/BannerSection"; // Temporarily removed
 
 const Index = () => {
   return (
     <div>
       {/* <HeroSlider /> */}
-      <BannerSection /> {/* Add the banner section here */}
+      {/* <BannerSection /> */} {/* Temporarily removed the banner section */}
       <div className="container mx-auto py-12 px-4 md:px-6">
         <div className="text-center space-y-4">
           <h1 className="text-4xl md:text-5xl font-poppins font-extrabold tracking-tight text-magenta">Welcome to TH-MED International</h1>
