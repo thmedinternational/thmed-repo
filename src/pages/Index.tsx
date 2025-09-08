3a3418bfce1ba2437b770b90cbf627f0b50bb1c2
[dyad] <sueguardmanagement@gmail.com>
2025-09-08 19:51:54 +0200

    [dyad] Removed the "Learn more" banners by deleting the `BannerSection` component and its usage from the `Index` page. - wrote 1 file(s), deleted 1 file(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,6 +1,5 @@
 import ProductList from "@/components/ProductList";
 import { HeroSlider } from "@/components/HeroSlider";
-import BannerSection from "@/components/BannerSection";
 import CategoryCarousel from "@/components/CategoryCarousel"; // Import the new category carousel
 
 const Index = () => {
@@ -8,7 +7,6 @@ const Index = () => {
     <div>
       <CategoryCarousel /> {/* Add the category carousel here */}
       <HeroSlider />
-      <BannerSection />
       <div className="container mx-auto py-12 px-4 md:px-6">
         <div className="text-center space-y-4">
           <h1 className="text-4xl md:text-5xl font-poppins font-extrabold tracking-tight text-magenta">Welcome to TH-MED International</h1>
